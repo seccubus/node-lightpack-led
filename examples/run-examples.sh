3b02c07da7910529220bc2eb4d3e4459c55f3ebb
Filip Lindqvist <buggfille@gmail.com>
2019-01-27 00:04:11 +0100

    Delete run-examples.sh

--- examples/run-examples.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export NODE_PATH=".:./node_modules"
-echo "*** get-Devices ***"
-node get-device.js
-echo "*** set different colors ***"
-node setled.js
-echo "*** Array test red,white,blue ***"
-node arraytest.js
